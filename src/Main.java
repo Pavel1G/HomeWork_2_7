adf652636e578ed9f24538925e4f04d1655916ec
Pavel Golubev <pavelgolubew@gmail.com>
2022-10-13 23:54:10 +0300

    HW part 2 are done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Queue<String> queue1 = new ArrayDeque<>(5);
+        Queue<String> queue2 = new ArrayDeque<>(5);
+
+        for (int i = 0; i < Math.floor(Math.random() * 5); i++) {
+            queue1.offer("Mike" + Integer.toString(i));
+        }
+        System.out.println(queue1);
+        System.out.println();
+        for (int i = 0; i < Math.floor(Math.random() * 5); i++) {
+            queue2.offer("Bob" + Integer.toString(i));
+        }
+        System.out.println(queue2);
+        System.out.println("----------------");
+
+        addHuman("John", queue1, queue2);
+        System.out.println(queue1);
+        System.out.println(queue2);
+    }
+
+    public static void addHuman(String name, Queue queue1, Queue queue2) {
+        if (queue1.size() == queue2.size() && queue1.size() == 5) {
+            System.out.println("Ххххххааааааля!");
+        } else if (queue1.size() < queue2.size()) {
+            queue1.add(name);
+        } else {
+            queue2.add(name);
+        }
     }
 }
